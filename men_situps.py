e7c233e8cbf7cfaf1c155674753c9def356e3d40
Beyazt43 <hasanbeyazitakpinar@gmail.com>
2026-07-31 23:09:45 +0300

    starting on the men's fitness dataset

--- men_situps.py
+++ men_situps.py
@@ -9,5 +9,8 @@ collected from twenty middle-aged men in a fitness club'."""
 X, y = datasets.load_linnerud(return_X_y=True, as_frame=False)
 # (20, 3) - 20 samples of exercise types Chins, Situps and Jumps, 3 features Weight, Waist and Pulse.
 
-print(X.shape)
-print(X[3])
+"""print(X.shape)
+print(X[3])"""
+
+X = X[:, 0]
+X = X.reshape((-1, 1))
