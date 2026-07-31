4fb3d6ccf194260fc2f89b5c24e9dfd428b6eeec
Beyazt43 <hasanbeyazitakpinar@gmail.com>
2026-07-31 23:41:06 +0300

    training and plotting

--- men_situps.py
+++ men_situps.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn import datasets, linear_model, model_selection
+from sklearn.linear_model import LinearRegression
 
 """This dataset has multiple targets: 'It consists of three exercise (data) and three physiological (target) variables
 collected from twenty middle-aged men in a fitness club'."""
@@ -12,5 +13,31 @@ X, y = datasets.load_linnerud(return_X_y=True, as_frame=False)
 """print(X.shape)
 print(X[3])"""
 
+# choosing the first feature (Chins) to plot against the target variables (Weight, Waist and Pulse)
 X = X[:, 0]
 X = X.reshape((-1, 1))
+
+# you can use print(y[:, 0].shape) to see the shape of the first target variable (Weight).
+
+
+# print(y.shape)
+
+X_train, X_test, y_train, y_test = model_selection.train_test_split(
+    X, y, test_size=0.33
+)
+
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+order = np.argsort(X_test[:, 0])
+
+plt.scatter(X_test[:, 0], y_test[:, 0], color="black", label="Actual")
+
+plt.plot(X_test[order, 0], y_pred[order, 0], color="red", label="Predicted")
+
+plt.xlabel("Chins")
+plt.ylabel("Weight")
+plt.legend()
+plt.show()
