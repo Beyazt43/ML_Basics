b2af62e75762cd977ffede50106977cfd5a283ed
Beyazt43 <hasanbeyazitakpinar@gmail.com>
2026-07-31 23:09:22 +0300

    starting on the men's fitness dataset

--- men_situps.py
+++ men_situps.py
@@ -2,5 +2,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 from sklearn import datasets, linear_model, model_selection
 
-"""his dataset has multiple targets: 'It consists of three exercise (data) and three physiological (target) variables
+"""This dataset has multiple targets: 'It consists of three exercise (data) and three physiological (target) variables
 collected from twenty middle-aged men in a fitness club'."""
+
+
+X, y = datasets.load_linnerud(return_X_y=True, as_frame=False)
+# (20, 3) - 20 samples of exercise types Chins, Situps and Jumps, 3 features Weight, Waist and Pulse.
+
+print(X.shape)
+print(X[3])
