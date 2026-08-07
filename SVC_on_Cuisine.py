fa831fcc0c9e986c16ade7fedae976b85bc01329
Beyazt43 <hasanbeyazitakpinar@gmail.com>
2026-08-07 11:32:21 +0300

    starting on SVC

---
+++ SVC_on_Cuisine.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import AdaBoostClassifier, RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import (
+    accuracy_score,
+    classification_report,
+    confusion_matrix,
+    precision_recall_curve,
+    precision_score,
+)
+from sklearn.model_selection import cross_val_score, train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+
+cuisines_df = pd.read_csv("../data/cleaned_cuisines.csv")
+cuisines_features_df = cuisines_df.drop(["Unnamed: 0", "cuisine"], axis=1)
+cuisines_label_df = cuisines_df["cuisine"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    cuisines_features_df, cuisines_label_df, test_size=0.3
+)
