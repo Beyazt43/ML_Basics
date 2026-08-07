75f4534ea293bac36f4eb98e110ce42e680e1899
Beyazt43 <hasanbeyazitakpinar@gmail.com>
2026-08-07 11:36:44 +0300

    now to K-Neighbors

--- SVC_on_Cuisine.py
+++ SVC_on_Cuisine.py
@@ -13,10 +13,27 @@ from sklearn.model_selection import cross_val_score, train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
 
-cuisines_df = pd.read_csv("../data/cleaned_cuisines.csv")
+cuisines_df = pd.read_csv("data/cleaned_cuisines.csv")
 cuisines_features_df = cuisines_df.drop(["Unnamed: 0", "cuisine"], axis=1)
 cuisines_label_df = cuisines_df["cuisine"]
 
 X_train, X_test, y_train, y_test = train_test_split(
     cuisines_features_df, cuisines_label_df, test_size=0.3
 )
+
+
+C = 10
+# Create different classifiers.
+classifiers = {
+    "Linear SVC": SVC(kernel="linear", C=C, probability=True, random_state=0)
+}
+
+n_classifiers = len(classifiers)
+
+for index, (name, classifier) in enumerate(classifiers.items()):
+    classifier.fit(X_train, np.ravel(y_train))
+
+    y_pred = classifier.predict(X_test)
+    accuracy = accuracy_score(y_test, y_pred)
+    print("Accuracy (train) for %s: %0.1f%% " % (name, accuracy * 100))
+    print(classification_report(y_test, y_pred))
