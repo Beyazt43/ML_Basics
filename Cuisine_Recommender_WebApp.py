f2de5c996924daa16b20fc304a74ecdb5e627ee3
Beyazt43 <hasanbeyazitakpinar@gmail.com>
2026-08-08 15:18:06 +0300

    ONNX Model created

--- Cuisine_Recommender_WebApp.py
+++ Cuisine_Recommender_WebApp.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from skl2onnx import convert_sklearn
+from skl2onnx.common.data_types import FloatTensorType
 from sklearn.metrics import (
     accuracy_score,
     classification_report,
@@ -23,3 +25,14 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
 
 model = SVC(kernel="linear", C=10, probability=True, random_state=0)
 model.fit(X_train, y_train.values.ravel())
+
+y_pred = model.predict(X_test)
+print(classification_report(y_test, y_pred))
+
+# This dataset has 380 ingredients listed, so you need to notate that number in FloatTensorType
+initial_type = [("float_input", FloatTensorType([None, 380]))]
+options = {id(model): {"nocl": True, "zipmap": False}}
+
+onx = convert_sklearn(model, initial_types=initial_type, options=options)
+with open("./model.onnx", "wb") as f:
+    f.write(onx.SerializeToString())
