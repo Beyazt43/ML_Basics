6ea6c113ab79763e0f89ce30b5a7aa9b61aa5158
Beyazt43 <hasanbeyazitakpinar@gmail.com>
2026-08-10 12:28:48 +0300

    TextBlob

--- intro_to_NLP.py
+++ intro_to_NLP.py
@@ -5,7 +5,7 @@ from textblob.np_extractors import ConllExtractor
 extractor = ConllExtractor()
 
 # later when you need a noun phrase extractor:
-user_input = input(" > ")
+"""user_input = input(" > ")
 print()
 print("Here are the noun phrases in your input:")
 print()
@@ -13,4 +13,24 @@ user_input_blob = TextBlob(
     user_input, np_extractor=extractor
 )  # note non-default extractor specified
 np = user_input_blob.noun_phrases
-print(np)
+print(np)"""
+
+# they removed the translate method from textblob
+"""blob = TextBlob(
+    "It is a truth universally acknowledged, that a single man in possession of a good fortune, must be in want of a wife!"
+)
+print(blob.translate(to="fr"))"""
+
+quote1 = """It is a truth universally acknowledged, that a single man in possession of a good fortune, must be in want of a wife."""
+
+quote2 = """Darcy, as well as Elizabeth, really loved them; and they were both ever sensible of the warmest gratitude towards the persons who, by bringing her into Derbyshire, had been the means of uniting them."""
+
+sentiment1 = TextBlob(quote1).sentiment
+sentiment2 = TextBlob(quote2).sentiment
+
+print(quote1 + " has a sentiment of " + str(sentiment1))
+print(quote2 + " has a sentiment of " + str(sentiment2))
+
+# blob.sentiment gives both polarity and subjectivity
+# but you can choose to get only one of them by
+# blob.sentiment.polarity or blob.sentiment.subjectivity
