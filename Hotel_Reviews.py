a37200bc1fc90b3019b00730ce4708ae87881395
Beyazt43 <hasanbeyazitakpinar@gmail.com>
2026-08-11 14:13:14 +0300

    Hotel_Reviews dataset

--- Hotel_Reviews.py
+++ Hotel_Reviews.py
@@ -37,3 +37,91 @@ for nat in nationality_freq[:10].index:
         + " reviews."
     )
 # freq.iloc was necessary to get the first row, not the one with the label 0
+
+
+# First create a new dataframe based on the old one, removing the uneeded columns
+hotel_freq_df = df.drop(
+    [
+        "Hotel_Address",
+        "Additional_Number_of_Scoring",
+        "Review_Date",
+        "Average_Score",
+        "Reviewer_Nationality",
+        "Negative_Review",
+        "Review_Total_Negative_Word_Counts",
+        "Positive_Review",
+        "Review_Total_Positive_Word_Counts",
+        "Total_Number_of_Reviews_Reviewer_Has_Given",
+        "Reviewer_Score",
+        "Tags",
+        "days_since_review",
+        "lat",
+        "lng",
+    ],
+    axis=1,
+)
+
+# Group the rows by Hotel_Name, count them and put the result in a new column Total_Reviews_Found
+"""hotel_freq_df["Total_Reviews_Found"] = hotel_freq_df.groupby("Hotel_Name").transform(
+    "count"
+)
+
+# Get rid of all the duplicated rows
+hotel_freq_df = hotel_freq_df.drop_duplicates(subset=["Hotel_Name"])
+hotel_freq_df = hotel_freq_df.sort_values(by="Total_Reviews_Found", ascending=False)"""
+
+hotel_freq_df = (
+    df.groupby("Hotel_Name")
+    .size()
+    .reset_index(name="Total_Reviews_Found")
+    .sort_values("Total_Reviews_Found", ascending=False)
+)
+print(hotel_freq_df)
+
+
+# define a function that takes a row and performs some calculation with it
+def get_difference_review_avg(row):
+    return row["Average_Score"] - row["Calc_Average_Score"]
+
+
+# 'mean' is mathematical word for 'average'
+df["Calc_Average_Score"] = round(
+    df.groupby("Hotel_Name").Reviewer_Score.transform("mean"), 1
+)
+
+# Add a new column with the difference between the two average scores
+df["Average_Score_Difference"] = df.apply(get_difference_review_avg, axis=1)
+
+# Create a df without all the duplicates of Hotel_Name (so only 1 row per hotel)
+review_scores_df = df.drop_duplicates(subset=["Hotel_Name"])
+
+# Sort the dataframe to find the lowest and highest average score difference
+review_scores_df = review_scores_df.sort_values(by=["Average_Score_Difference"])
+
+print(
+    review_scores_df[
+        [
+            "Average_Score_Difference",
+            "Average_Score",
+            "Calc_Average_Score",
+            "Hotel_Name",
+        ]
+    ]
+)
+
+
+# without lambdas (using a mixture of notations to show you can use both)
+start = time.time()
+no_negative_reviews = sum(df.Negative_Review == "No Negative")
+print("Number of No Negative reviews: " + str(no_negative_reviews))
+
+no_positive_reviews = sum(df["Positive_Review"] == "No Positive")
+print("Number of No Positive reviews: " + str(no_positive_reviews))
+
+both_no_reviews = sum(
+    (df.Negative_Review == "No Negative") & (df.Positive_Review == "No Positive")
+)
+print("Number of both No Negative and No Positive reviews: " + str(both_no_reviews))
+
+end = time.time()
+print("Sum took " + str(round(end - start, 2)) + " seconds")
