6bb3ff0bfde40870c418121b82a987b21873473d
Beyazt43 <hasanbeyazitakpinar@gmail.com>
2026-08-13 10:44:13 +0300

    reviews filtered and put into a different csv for NLP uses later

--- Hotel_Reviews_Filtered.py
+++ Hotel_Reviews_Filtered.py
@@ -7,6 +7,20 @@ from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
 nltk.download("stopwords")
 nltk.download("vader_lexicon")
+
+vader_sentiment = SentimentIntensityAnalyzer()
+
+
+# There are 3 possibilities of input for a review:
+# It could be "No Negative", in which case, return 0
+# It could be "No Positive", in which case, return 0
+# It could be a review, in which case calculate the sentiment
+def calc_sentiment(review):
+    if review == "No Negative" or review == "No Positive":
+        return 0
+    return vader_sentiment.polarity_scores(review)["compound"]
+
+
 df = pd.read_csv("data/Hotel_Reviews_Filtered.csv")
 # We want to find the most useful tags to keep
 # Remove opening and closing brackets
@@ -54,6 +68,48 @@ def remove_stopwords(review):
 # Remove the stop words from both columns
 df.Negative_Review = df.Negative_Review.apply(remove_stopwords)
 df.Positive_Review = df.Positive_Review.apply(remove_stopwords)
+
+print("Saving results to Hotel_Reviews_NLP.csv")
+df.to_csv(r"data/Hotel_Reviews_NLP.csv", index=False)
+
+
+# Add a negative sentiment and positive sentiment column
+print("Calculating sentiment columns for both positive and negative reviews")
+start = time.time()
+df["Negative_Sentiment"] = df.Negative_Review.apply(calc_sentiment)
+df["Positive_Sentiment"] = df.Positive_Review.apply(calc_sentiment)
+end = time.time()
+print("Calculating sentiment took " + str(round(end - start, 2)) + " seconds")
+
+df = df.sort_values(by=["Negative_Sentiment"], ascending=True)
+print(df[["Negative_Review", "Negative_Sentiment"]])
+df = df.sort_values(by=["Positive_Sentiment"], ascending=True)
+print(df[["Positive_Review", "Positive_Sentiment"]])
+
+# Reorder the columns (This is cosmetic, but to make it easier to explore the data later)
+df = df.reindex(
+    [
+        "Hotel_Name",
+        "Hotel_Address",
+        "Total_Number_of_Reviews",
+        "Average_Score",
+        "Reviewer_Score",
+        "Negative_Sentiment",
+        "Positive_Sentiment",
+        "Reviewer_Nationality",
+        "Leisure_trip",
+        "Couple",
+        "Solo_traveler",
+        "Business_trip",
+        "Group",
+        "Family_with_young_children",
+        "Family_with_older_children",
+        "With_a_pet",
+        "Negative_Review",
+        "Positive_Review",
+    ],
+    axis=1,
+)
 
 print("Saving results to Hotel_Reviews_NLP.csv")
 df.to_csv(r"data/Hotel_Reviews_NLP.csv", index=False)
